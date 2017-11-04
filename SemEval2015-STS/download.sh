ea0c7452b3939443a6b92888992b074bb950865f
Michael Tu <tuzhucheng@outlook.com>
2017-11-04 01:23:00 -0400

    Add SemEval 2015 STS task data (#25)

---
+++ SemEval2015-STS/download.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget https://github.com/brmson/dataset-sts/raw/master/data/sts/semeval-sts/all/2015.test.tsv
+wget https://github.com/brmson/dataset-sts/raw/master/data/sts/semeval-sts/all/2015.train.tsv
+wget https://github.com/brmson/dataset-sts/raw/master/data/sts/semeval-sts/all/2015.val.tsv
